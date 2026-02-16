99df2b10d9e68f82a1a9a1a116b5793a312fbc2e
chasebfreeman <chase@elitemotorsportsllc.com>
2026-02-16 16:43:54 +0000

    _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
-
-import { Stack } from 'expo-router';
-import { SupabaseAuthProvider } from '@/contexts/SupabaseAuthContext';
-import React from 'react';
+import "react-native-gesture-handler";
+import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Stack } from "expo-router";
+import { SupabaseAuthProvider } from "@/contexts/SupabaseAuthContext";
 
 export default function RootLayout() {
   return (
-    <SupabaseAuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="auth" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </SupabaseAuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SupabaseAuthProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="auth" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SupabaseAuthProvider>
+    </GestureHandlerRootView>
   );
 }
